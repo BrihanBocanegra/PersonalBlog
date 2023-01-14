62db63a9932f2181f175ca9851f518c1da86c50e
Brihan Bocanegra <brihan.bocanegra@outlook.com>
2023-01-14 03:48:21 -0500

    Add project files.

---
+++ BrihanBocanegra/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using Azure.Identity;
+using BrihanBocanegra.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.KeyVault;
+using MimeKit;
+using System;
+using System.Threading.Tasks;
+using Azure.Security.KeyVault.Secrets;
+
+
+namespace BrihanBocanegra.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        // Acción que procesa el formulario
+        public async Task<IActionResult> SendMessage(ContactFormModel model)
+        {
+
+            if (ModelState.IsValid)
+            {
+                var MyEmail = "me@brihanbocanegra.com";
+                var Password = "******";
+                // Procesar los datos del formulario aquí, como enviar un correo electrónico o guardarlos en una base de datos.
+                var emailMessage = new MimeMessage();
+
+                emailMessage.From.Add(new MailboxAddress("Contact Form", MyEmail));
+                emailMessage.To.Add(new MailboxAddress("Receiver", model.Email));
+                //emailMessage.Cc.Add(new MailboxAddress("", MyEmail));
+                emailMessage.Subject = "Mensaje desde el formulario de contacto";
+                emailMessage.Body = new TextPart("plain") {
+                    Text = 
+                    "Correo: " + model.Email + Environment.NewLine +
+                    "Teléfono: " + model.Phone + Environment.NewLine +
+                    "Asunto: " + model.Subject + Environment.NewLine +
+                    "Mensaje: " + model.Message
+                };
+                
+                using (var client = new MailKit.Net.Smtp.SmtpClient())
+                {
+                    await client.ConnectAsync("smtp.office365.com", 587);
+                    await client.AuthenticateAsync(MyEmail, Password);
+                    await client.SendAsync(emailMessage);
+                    await client.DisconnectAsync(true);
+                }
+
+
+                return RedirectToAction("Index");
+            }
+
+            return View(model);
+        }
+    }
+
+    
+
+}
---
+++ BrihanBocanegra/Models/ContactFormModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Runtime.CompilerServices;
+
+namespace BrihanBocanegra.Models
+{
+    public class ContactFormModel
+    {
+        [Required]
+        [StringLength(50)]
+        public string Subject { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(18)]
+        public string Phone { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string Message { get; set; }
+    }
+
+}
